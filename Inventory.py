928d89f9cb1ef4aac4ee3efddd6204f584908345
cbjfan2009 <cbjfan2009@gmail.com>
2021-04-16 11:32:42 -0400

    Cannot figure out my SQL error with "ADD INVENTORY" function

--- Inventory.py
+++ Inventory.py
@@ -15,9 +15,10 @@ def add_inventory_count(item_count, item_num):
     conn.commit()
 
 def add_item(itemNumber,prodDescrip,weight,length, width, height,count): #Item-Number, Description, Weight, Package-Length,  Package-Width, Package-Height, Inventory-Count
-    item_values = (itemNumber,prodDescrip,weight,length,width,height,count)
-    sql_text = f"INSERT INTO WEB_INVENTORY(ItemNum,Description,Weight,PkgL,PkgW,PkgH,InventoryCount) VALUES({itemNumber})"
-    cursor.execute(sql_text)
+    item_values = (itemNumber, prodDescrip, weight, length, width, height, count)
+    #sql_text = "INSERT INTO WEB_INVENTORY (ItemNum, Description, Weight, PkgL, PkgW, PkgH, InventoryCount) VALUES (" + itemNumber + ", " + prodDescrip + ", " + weight + ", " + length + ", " + width + ", " + height + ", " + count + ");"
+    sql = "INSERT INTO WEB_INVENTORY (ItemNum, Description, Weight, PkgL, PkgW, PkgH, InventoryCount) VALUES (" + itemNumber + ", " + prodDescrip + ", " + weight + ", " + length + ", " + width + ", " + height + ", " + count + ")"
+    cursor.execute(sql)
     conn.commit()
 
 # flask routes setup
@@ -32,15 +33,17 @@ def main():
         data = cur.fetchall()
         return render_template('index.html', data=data) #=enumerate(data)
 
+
+##############################Currently Working On This Route#####################
 @app.route("/add_inventory.html", methods=['POST', 'GET'])
 def add_inventory():
     if request.method == 'POST':
-        itemNumber = int(request.form['itemNumber'])
+        itemNumber = str(request.form['itemNumber'])
         prodDescrip = str(request.form['prodDescrip'])
-        weight = int(request.form['weight'])
-        length = int(request.form['length'])
-        height = float(request.form['height'])
-        invCount = float(request.form['invCount'])
+        weight = str(request.form['weight'])
+        length = str(request.form['length'])
+        height = str(request.form['height'])
+        invCount = str(request.form['invCount'])
         add_item(itemNumber, prodDescrip, weight, length, height, invCount)
         return render_template(('add_inventory.html', ))
 
@@ -64,9 +67,14 @@ query = cursor.execute('Select * FROM WEB_INVENTORY WHERE InventoryCount !=0')
 for row in query:
     print("Item ID: ", row[0], " ", "Item Name: ", row[1], " ", "Item Inventory Count: ", row[6])
 
+#add_item("10101010101", "TESTING", "20","12", "12", "12", "58008")
 
-#add_inventory(987654321, 9001203)
+
 #print("------------------------NEW QUERY------------------")
+
+sql = "INSERT INTO Web_Inventory (ItemNum, Description, Weight, PkgL, PkgW, PkgH, InventoryCount) VALUES (0000001, 0000001, 100, 12, 12, 12, 55555)"
+cursor.execute(sql)
+
 
 #querytest = cursor.execute('Select * FROM WEB_INVENTORY WHERE InventoryCount !=0')
 #for row in query:
