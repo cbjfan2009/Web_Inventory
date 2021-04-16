335823d619564ba8d89355ce4c3e21a0b33d8436
cbjfan2009 <cbjfan2009@gmail.com>
2021-04-16 06:54:34 -0400

    More changes to add_inventory route...still can't get the SQL to run correctly to UPDATE the DB

--- Inventory.py
+++ Inventory.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 import _sqlite3
 
 #connection to DB
@@ -7,17 +7,17 @@ conn = _sqlite3.connect('Inventory.db')
 #setup cursor object
 cursor = conn.cursor()
 
-def add_inventory(item_count, item_num):
+def add_inventory_count(item_count, item_num):
     update_string = str(
     "Update WEB_INVENTORY SET InventoryCount =" + str(item_count)+
     " WHERE ITEMNUM = " + str(item_num))
     cursor.execute(update_string)
     conn.commit()
 
-def add_item(new_item): #Item-Number, Description, Weight, Package-Length,  Package-Width, Package-Height, Inventory-Count
-    cursor.execute(
-    '''INSERT INTO WEB_INVENTORY(ItemNum,Description,Weight,PkgL,PkgW,PkgH,InventoryCount) 
-    VALUES(?, ?, ?, ?, ?, ?)''', new_item)
+def add_item(itemNumber,prodDescrip,weight,length, width, height,count): #Item-Number, Description, Weight, Package-Length,  Package-Width, Package-Height, Inventory-Count
+    item_values = (itemNumber,prodDescrip,weight,length,width,height,count)
+    sql_text = f"INSERT INTO WEB_INVENTORY(ItemNum,Description,Weight,PkgL,PkgW,PkgH,InventoryCount) VALUES({itemNumber})"
+    cursor.execute(sql_text)
     conn.commit()
 
 # flask routes setup
@@ -30,13 +30,39 @@ def main():
         cur = con.cursor()
         cur.execute('Select * FROM WEB_INVENTORY')
         data = cur.fetchall()
-        return render_template('main.html', data=data) #=enumerate(data)
+        return render_template('index.html', data=data) #=enumerate(data)
 
+@app.route("/add_inventory.html", methods=['POST', 'GET'])
+def add_inventory():
+    if request.method == 'POST':
+        itemNumber = int(request.form['itemNumber'])
+        prodDescrip = str(request.form['prodDescrip'])
+        weight = int(request.form['weight'])
+        length = int(request.form['length'])
+        height = float(request.form['height'])
+        invCount = float(request.form['invCount'])
+        add_item(itemNumber, prodDescrip, weight, length, height, invCount)
+        return render_template(('add_inventory.html', ))
 
-#query = cursor.execute('Select * FROM WEB_INVENTORY WHERE InventoryCount !=0')
+    else:
+        return render_template('add_inventory.html')
 
-#for row in query:
- #   print("Item ID: ", row[0], " ", "Item Name: ", row[1], " ", "Item Inventory Count: ", row[6])
+@app.route("/delete_inventory.html")
+def delete_inventory():
+    return render_template(('delete_inventory.html'))
+
+@app.route("/update_specifications.html")
+def update_specifications():
+    return render_template(('update_specifications.html'))
+
+@app.route("/update_inventory.html")
+def update_inventory():
+    return render_template(('update_inventory.html'))
+
+query = cursor.execute('Select * FROM WEB_INVENTORY WHERE InventoryCount !=0')
+
+for row in query:
+    print("Item ID: ", row[0], " ", "Item Name: ", row[1], " ", "Item Inventory Count: ", row[6])
 
 
 #add_inventory(987654321, 9001203)
